80d8ca9c08299a1f5636f0acf0b044650833aaba
Norbert Konefał <norbertkonefa@outlook.com>
2020-12-10 17:54:30 +0100

    Wiadomosc_NR_2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int a = 2;
+	int b=2;
+	int wynik=a+b;
+        System.out.println(wynik);
     }
 }
